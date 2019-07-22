41d4a54f29da56a0d6c03b72517d365427e770f0
Fhranko <francohuanaco@gmail.com>
2019-07-22 10:23:26 -0400

    First Commit

---
+++ save_task.php
@@ -0,0 +1,22 @@
+<?php
+
+include("db.php");
+
+    if(isset($_POST['save_task'])){
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        
+        $query = "INSERT INTO task(title, description)
+            values ('$title', '$description')";
+
+        $result = mysqli_query($conn, $query);
+        if (!$result){
+            die("Query fail");
+        }
+        
+        $_SESSION['message'] = 'Task Saved Succesfully fully';
+        $_SESSION['color-message'] = 'success';
+
+        header("Location: index.php");
+    }
+?>
